e86348d6f9792be242917ed429e195ae33d652bb
daniel watson <ozzloy@each.do>
2024-10-31 12:53:32 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20241031195031-demo-user.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  // define your schema in options object
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate(
+      [
+        {
+          email: "demo@example.com",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "user1@example.com",
+          username: "FakeUser1",
+          hashedPassword: bcrypt.hashSync("password2"),
+        },
+        {
+          email: "user2@example.com",
+          username: "FakeUser2",
+          hashedPassword: bcrypt.hashSync("password3"),
+        },
+      ],
+      { validate: true },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+      },
+      {},
+    );
+  },
+};
